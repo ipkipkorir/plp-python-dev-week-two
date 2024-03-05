61573ae9a00e547bc438fb762293d1dcd518a618
montfortkorir <ipkipkorir@gmail.com>
2024-03-05 14:56:04 +0300

    Added task number 2.

---
+++ tuple_of_books.py
@@ -0,0 +1,11 @@
+'''
+Create the tuple of favourite books.
+Use a for-loop to print each book in the tuple in a new line.
+'''
+
+fav_books = ("Genesis", "Exodus", "Tobit", "Jonah", "Judith",)
+print("===============")
+for book in fav_books:
+    print(book)
+
+print("===============")
